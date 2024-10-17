95fa57b330904e707f82f1ec6189ecf666e27dfb
Nguyen Van Phuc <nguyenphuc02042004@gmail.com>
2024-10-17 21:31:22 +0700

    Initial commit

---
+++ constant/constants.js
@@ -0,0 +1,9 @@
+// constants.js
+const CLIENT_ID = '7edacf5546fa4bb58e20f54e69e47079';
+const CLIENT_SECRET = 'e538099ccc6b4da38cf1723b74542411';
+const API_URL = 'https://api.spotify.com/v1/search';
+const TOKEN_URL = 'https://accounts.spotify.com/api/token';
+const LIMIT = 20;
+
+export { API_URL, CLIENT_ID, CLIENT_SECRET, LIMIT, TOKEN_URL };
+
